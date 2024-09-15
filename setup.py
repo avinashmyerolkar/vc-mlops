88854af4580aaf34e599cc2bf8fee36f919c6574
avinashmyerolkar <avinashmyerolkar@gmail.com>
2024-09-15 20:07:56 +0530

    add cookiecutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='hands on version control in mlops via emotion detection through text',
+    author='avinash-m-yerolkar',
+    license='',
+)
